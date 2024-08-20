ed29ee6df647f6681c3112731ef1237e47bb1e60
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-08-20 23:52:36 +0800

    get access token

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 //create a server 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Report Automation for TWS with Hubspot and RingCentral! Your server is running.');
@@ -12,14 +13,67 @@ app.get('/', (req, res) => {
 // Middleware to parse JSON request body
 app.use(express.json());
 
-//listen for any webhooks 
 
+const RC_SDK = require('@ringcentral/sdk').SDK;
+var rcsdk = new RC_SDK({
+  'server': process.env.RC_SERVER_URL,
+  'clientId': process.env.RC_CLIENT_ID,
+  'clientSecret': process.env.RC_CLIENT_SECRET
+});
 
+var platform = rcsdk.platform();
 
-const port = process.env.PORT || 8080;
+/* Authenticate a user using a personal JWT token */
+const authenticate = async () => {
+  try { 
+    await platform.login({'jwt': process.env.RC_JWT});
+
+    platform.on(platform.events.loginSuccess, function(e){
+        read_user_calllog();
+    });
+
+    platform.on(platform.events.loginError, function(e){
+        console.log("Unable to authenticate to platform. Check credentials.", e.message)
+        process.exit(1)
+    });
+        
+    
+    console.log("Successfully Authenticated with JWT");
+  } catch (error) {
+    console.log('Error during JWT authentication:', error);
+  }
+}
+
+const read_user_calllog = async () => {
+  try {
+    let queryParams = {
+      
+    }
+    const response = await platform.get("/restapi/v1.0/account/~/extension/~/call-log");
+    return response;
+    
+  } catch (error) {
+    
+  }
+}
+
+// try {
+//   var resp = await platform.post('/restapi/v1.0/account/~/extension/~/ring-out', {
+//     'from': { 'phoneNumber': "<YOUR RINGCENTRAL PHONE NUMBER>" },
+//     'to':   { 'phoneNumber': "<THE PHONE NUMBER YOU ARE CALLING" },
+//     'playPrompt': false
+//   })
+//   var jsonObj = await resp.json()
+//   console.log("Call placed. Call status: " + jsonObj.status.callStatus)
+// } catch (e) {
+//   console.log("Unable to place a ring-out call.", e.message)
+// }
+
 
 app.listen(port, () =>{
   console.log(`Server is running on port ${port}`);
 })
+
+authenticate();
 
 
