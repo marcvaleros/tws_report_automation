e965a2f8cf091961ff103f79779235cdf4d04e24
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-09-25 10:35:20 +0800

    updated production env and db config

--- database.js
+++ database.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+let db;
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOSTNAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME
-})
+// defaults to development
+const isProduction = process.env.NODE_ENV === 'production'; 
+
+if (isProduction) {
+  db = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+} else {
+  db = mysql.createConnection({
+    host: process.env.DB_HOSTNAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME,
+  });
+}
+
 
 db.connect((err) => {
   if(err){
