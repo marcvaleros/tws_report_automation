eed3535491eb22ed81d81c72ca87932b9ae82662
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-09-30 21:01:33 +0800

    implemented pooling connections for automatic reconnection

--- database.js
+++ database.js
@@ -1,35 +1,35 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-  // db = mysql.createConnection({
-  //   host: process.env.DB_HOSTNAME,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD || '',
-  //   database: process.env.DB_NAME,
-  // });
+// const db = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 
-function handleDisconnect (){
-  db.connect((err) => {
-    if(err){
-      console.error('Error Connecting to MySQL:', err);
-      setTimeout(handleDisconnect,2000); 
-    }else{
-      console.log('Connected to MySQL Database');
-    }
-  });
+// function handleDisconnect (){
+//   db.connect((err) => {
+//     if(err){
+//       console.error('Error Connecting to MySQL:', err);
+//       setTimeout(handleDisconnect,2000); 
+//     }else{
+//       console.log('Connected to MySQL Database');
+//     }
+//   });
 
-  db.on('error', function(err) {
-    console.log('Database Error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST'){
-      handleDisconnect();
-    }else{
-      throw err;
-    }
-  })
-}
+//   db.on('error', function(err) {
+//     console.log('Database Error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+//       handleDisconnect();
+//     }else{
+//       throw err;
+//     }
+//   })
+// }
 
-handleDisconnect();
+// handleDisconnect();
 
+const db = mysql.createPool({
+  uri: process.env.CLEARDB_DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 module.exports = db;
